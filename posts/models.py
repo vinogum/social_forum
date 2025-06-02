480a27466b45a4c15a1f21aabe44b6370307d54c
nikolay_vynohradov <vinogum@gmail.com>
2025-06-02 15:07:48 +0300

    Add initial models and utilities.py file

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,34 @@
 from django.db import models
+from .utilities import upload_to
+from django.contrib.auth.models import User
 
 
-# Here we define the Post model
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    title = models.CharField(null=False, blank=False)
+    text = models.TextField(null=False, blank=False)
+    image = models.ImageField(upload_to=upload_to, null=True, blank=True)
+    created_at = models.DataTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    text = models.TextField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Reaction(models.Model):
+    LIKE = 1
+    DISLIKE = -1
+    NONE = 0
+
+    REACTION_CHOICES = [
+        (LIKE, "Like"),
+        (DISLIKE, "Dislike"),
+        (NONE, "None"),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reactions")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="reactions")
+    reaction = models.IntegerField(choices=REACTION_CHOICES, default=NONE)
---
+++ posts/utilities.py
@@ -0,0 +1,2 @@
+def upload_to(instance, filename):
+    return f"user_{instance.user.id}/{filename}"
