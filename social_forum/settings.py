b04eb30e3675b9eae7b4df77ee9e860a5539e0d7
nikolay_vynohradov <vinogum@gmail.com>
2025-07-15 19:47:43 +0300

    Make django secret key public

--- social_forum/settings.py
+++ social_forum/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("DJANGO_SECRET_KEY")
+SECRET_KEY = "django-insecure-q)+k+w)p*o3dha+0vhhh5kez0p*&n+#*m24vdlvk=z_gle+2!c"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
